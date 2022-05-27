5525fccf0e1fb8021578f2298f14312fd352e0de
ildar.dautov01@gmail.com <Nizamova58>
2022-05-27 04:16:57 +0300

    test

---
+++ src/app/common/services/destroy.service.spec.ts
@@ -0,0 +1,39 @@
+import {Observable, of, timer} from 'rxjs';
+import {finalize, takeUntil} from 'rxjs/operators';
+
+import { DestroyService } from './destroy.service';
+
+describe('DestroyService', () => {
+  let service: DestroyService;
+
+  beforeEach(() => {
+    service = new DestroyService();
+  });
+
+  it('keeps subscription when alive', () => {
+    let result = 'initial';
+    const observable$: Observable<string> = of<string>('test');
+
+    observable$.pipe(takeUntil(service)).subscribe(data => {
+      result = data;
+    });
+
+    expect(result).toBe('test');
+  });
+
+  it('completes Observable on destroy', () => {
+    let result = false;
+
+    const observable$: Observable<any> = timer(2000).pipe(
+      takeUntil(service),
+      finalize(() => {
+        result = true;
+      }),
+    );
+
+    observable$.subscribe();
+    service.ngOnDestroy();
+
+    expect(result).toBe(true);
+  });
+});
